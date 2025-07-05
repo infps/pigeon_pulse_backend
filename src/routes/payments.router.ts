66c091f398c2b75a18b6c4b44b3458a6a5956e00
Tushar Daiya <tushardaiya@outlook.com>
2025-07-05 08:38:33 +0530

    remove old controllers

--- src/routes/payments.router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { authMiddleware } from "../middlewares/auth.middleware";
-import { getPayments } from "../controllers/payments.controller";
-
-export const paymentsRouter = Router();
-
-paymentsRouter.get("/", authMiddleware, getPayments);
