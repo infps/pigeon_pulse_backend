a256fda68beb01ce558df34124352fdd21e177cc
Tushar Daiya <tushardaiya@outlook.com>
2025-07-14 21:06:33 +0530

    feat: rename and implement fetchRaceItemResult, add additional endpoints for breeders, race results, event inventory, and events

--- src/routes/firebird.ts
+++ src/routes/firebird.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import { fetchFirebirdData } from "../controllers/firebird.controller";
+import {
+  fetchBreeders,
+  fetchEventInventory,
+  fetchEvents,
+  fetchRaceItemResult,
+  fetchRaceResult,
+} from "../controllers/firebird.controller";
 
 const fireBirdRouter = Router();
 
-fireBirdRouter.get("/", fetchFirebirdData);
+fireBirdRouter.get("/race-item", fetchRaceItemResult);
+fireBirdRouter.get("/breeders", fetchBreeders);
+fireBirdRouter.get("/race-result", fetchRaceResult);
+fireBirdRouter.get("/event-inventory", fetchEventInventory);
+fireBirdRouter.get("/events", fetchEvents);
 
 export default fireBirdRouter;
