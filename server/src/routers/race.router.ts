1bac14d960bdd1dddcc63e3e874c624154b89398
Tushar Daiya <tushardaiya@outlook.com>
2025-08-13 14:03:02 +0530

    feat: enhance race management with detailed race retrieval and item handling

--- server/src/routers/race.router.ts
+++ server/src/routers/race.router.ts
@@ -1,10 +1,37 @@
 import { Router } from "express";
 import { requireRole } from "../middlewares/auth.middleware";
-import { createRace, listRaces } from "../controllers/race.controller";
+import {
+  createRace,
+  listRace,
+  listRaceItems,
+  listRaceResults,
+  listRaces,
+  publishRaceResults,
+  raceItemLoftBasket,
+  raceItemRaceBasket,
+} from "../controllers/race.controller";
 
 const raceRouter = Router();
 
 raceRouter.post("/", requireRole(["ADMIN"]), createRace);
 raceRouter.get("/event/:id", requireRole(["ADMIN"]), listRaces);
+raceRouter.get("/:id", requireRole(["ADMIN"]), listRace);
+raceRouter.get("/:id/items", requireRole(["ADMIN"]), listRaceItems);
+raceRouter.post(
+  "/:id/item/loft-basket",
+  requireRole(["ADMIN"]),
+  raceItemLoftBasket
+);
+raceRouter.post(
+  "/:id/item/race-basket",
+  requireRole(["ADMIN"]),
+  raceItemRaceBasket
+);
+raceRouter.post(
+  "/:id/publish-result",
+  requireRole(["ADMIN"]),
+  publishRaceResults
+);
+raceRouter.get("/:id/results", requireRole(["ADMIN"]), listRaceResults);
 
 export default raceRouter;
